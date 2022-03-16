95cb231b5f9396772fefc564e450a3119681e6e2
Mohamed Muntasir <devmotheg@gmail.com>
2022-03-16 17:31:48 +0300

    Add markdown example

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -6,8 +6,66 @@
 import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
+const initialMarkdown = `
+# Markdown syntax guide
+
+## Headers
+
+# This is a Heading h1
+## This is a Heading h2
+###### This is a Heading h6
+
+## Emphasis
+
+*This text will be italic*
+_This will also be italic_
+
+**This text will be bold**
+__This will also be bold__
+
+_You **can** combine them_
+
+## Lists
+
+### Unordered
+
+* Item 1
+* Item 2
+* Item 2a
+* Item 2b
+
+### Ordered
+
+1. Item 1
+1. Item 2
+1. Item 3
+  1. Item 3a
+  1. Item 3b
+
+## Links
+
+You may be using [Markdown Live Preview](https://markdownlivepreview.com/).
+
+## Blockquotes
+
+> Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber with Aaron Swartz.
+>
+>> Markdown is often used to format readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.
+
+## Blocks of code
+
+\`\`\`
+let message = 'Hello world';
+alert(message);
+\`\`\`
+
+## Inline code
+
+\`console\` object in JS isn't just for \`.log()\`s.
+`;
+
 const Project = () => {
-	const [markdown, setMarkdown] = useState("");
+	const [markdown, setMarkdown] = useState(initialMarkdown);
 
 	return (
 		<>
